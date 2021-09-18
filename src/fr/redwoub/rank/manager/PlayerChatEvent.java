b1ac1ce5895df97ca638e4aaa4736fcbda9e429f
Redwoub <eloiverinaudcoma@gmail.com>
2021-09-18 19:16:20 +0100

    [add] chat color

--- src/fr/redwoub/rank/manager/PlayerChatEvent.java
+++ src/fr/redwoub/rank/manager/PlayerChatEvent.java
@@ -1,6 +1,19 @@
 package fr.redwoub.rank.manager;
 
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 public class PlayerChatEvent implements Listener {
+
+    @EventHandler
+    public void chatEvent(AsyncPlayerChatEvent event){
+        Player player = event.getPlayer();
+        String value = ChatColor.translateAlternateColorCodes('&', event.getMessage());
+        if(player.hasPermission("chatcolor.use")){
+            event.setMessage(value);
+        }
+    }
 }
